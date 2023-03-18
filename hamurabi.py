f1ba806a9ec3b3ffdb480d7562baa7cae705568a
dhannyamohangeetha <dhannyabrijesh.ktm@gmail.com>
2023-03-18 09:49:05 -0400

    outline created for hamurabi game

--- hamurabi.py
+++ hamurabi.py
@@ -1,1 +1,45 @@
-abc
+class Hamurabi():
+    def play_game(self):
+        print("Let's play!")
+
+# hamurabi = Hamurabi()
+# hamurabi.play_game()
+def askhowmanyAcresToBuy(price:int,bushels:int):
+  return pass
+
+def askHowManyAcresToSell(acresOwned:int):
+    return pass
+
+def askHowMuchGrainToFeedPeople(bushels:int):
+    return pass
+
+def askHowManyAcresToPlant(acresOwned:int, population:int,bushels:int):
+    return pass
+
+def plagueDeaths(population:int):
+    return pass
+
+def starvationDeaths(population:int,bushelsFedToPeople:int):
+    return pass
+
+def uprising(population:int,howManyPeopleStarved:int): #return boolean
+    return pass
+
+def immigrants(population:int,acresOwned:int,grainInStorage:int):
+    return pass
+
+def harvest(acres:int,bushelsUsedAsSeed:int):
+    return pass
+
+def grainEatenByRats(bushels:int):
+    return pass
+
+def newCostOfLand():
+    return pass
+def printSummary():
+    return pass
+
+def final_Summary():
+    return pass
+
+
